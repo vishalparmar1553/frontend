fa75e95427ce93edccee421148cb04c9f0594096
vishalparmar1553 <53140046+vishalparmar1553@users.noreply.github.com>
2025-09-04 16:47:45 +0530

    error fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const local = "http://localhost:5000";
-const production = process.env.REACT_APP_API_BACKEDN;
+const production = "https://backend-2ywi.onrender.com";
 const api = axios.create({
   baseURL: `${production}/api`,
 });
